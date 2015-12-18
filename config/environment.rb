03dfd16b7f2b5242b0e343230b62cf1445b45c2a
Diogo <salioudiogo@gmail.com>
2015-12-18 03:31:35 +0100

    Depot Initial de not appli exemple

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+# Config gravatar image
+config.gem 'gravatar_image_tag'
